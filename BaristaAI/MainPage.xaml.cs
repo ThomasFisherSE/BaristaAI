828cf1a4363d9c432a3f312bcf53b56be6a9d25e
Thomas Fisher <t.d.fisher.se@gmail.com>
2025-03-05 17:20:00 +0000

    MainPage exception handling and refactoring

--- BaristaAI/MainPage.xaml.cs
+++ BaristaAI/MainPage.xaml.cs
@@ -12,17 +12,45 @@ namespace BaristaAI
             BindingContext = _viewModel = viewModel;
         }
 
-        private async void OnSubmitClicked(object sender, EventArgs e)
+        private void OnSubmitClicked(object sender, EventArgs e)
         {
-            string prompt = PromptEntry.Text;
+            _ = SubmitPrompt(PromptEntry.Text);
+        }
 
+        private async Task<bool> SubmitPrompt(string prompt)
+        {
             // Disable the UI while waiting for the response
+            BlockNewPrompts();
+
+            var success = true;
+            try
+            {
+                await _viewModel.GetChatResponse(prompt);
+            }
+            catch (Exception exception)
+            {
+                success = false;
+                await Console.Error.WriteLineAsync(
+                    $"An unexpected error occurred while getting a chat response: {exception}");
+            }
+            finally
+            {
+                // Revert UI back to ready state
+                AllowNewPrompts();
+            }
+
+            return success;
+        }
+
+        private void BlockNewPrompts()
+        {
             SubmitButton.IsEnabled = false;
             SubmitButton.Text = "I'm thinking about it, please wait...";
             PromptEntry.IsEnabled = false;
-            await _viewModel.GetChatResponse(prompt);
+        }
 
-            // Revert UI back to ready state
+        private void AllowNewPrompts()
+        {
             SubmitButton.Text = "Submit";
             SubmitButton.IsEnabled = true;
             PromptEntry.Text = string.Empty;

ab5be30f8f48da1fcf29ba9fcfa3df0a7e4a1fdd
Thomas Fisher <t.d.fisher.se@gmail.com>
2025-03-05 21:54:26 +0000

    Added classes for representing LLM content generation responses

---
+++ BaristaAI/Model/GenerateContentResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace BaristaAI.Model;
+
+public abstract class GenerateContentResponse(string? error = null, bool isValid = true)
+{
+    public string? Error { get; set; } = error;
+
+    public bool IsValid { get; set; } = isValid;
+}
---
+++ BaristaAI/Model/GenerateTextResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace BaristaAI.Model;
+
+public class GenerateTextResponse(string? text, string? error = null, bool isValid = true) 
+    : GenerateContentResponse(error, isValid)
+{
+    public string? Text { get; set; } = text;
+}

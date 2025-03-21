49349c5873cfb9cb6feab86b988d9fd74dc41f5b
Thomas Fisher <t.d.fisher.se@gmail.com>
2025-03-21 16:32:05 +0000

    Initial view setup for coffee preference inputs

---
+++ BaristaAI/View/CoffeePreferenceInputs.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaristaAI.View;
+
+public partial class CoffeePreferenceInputs : ContentView
+{
+    public CoffeePreferenceInputs()
+    {
+        InitializeComponent();
+    }
+}
